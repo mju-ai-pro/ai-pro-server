e6b1135b34a955e0c292947de659fb77d147cb2b
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-10-07 15:18:51 +0900

    cors all

--- src/main/java/teamproject/AIPro/domain/member/controller/ChatController.java
+++ src/main/java/teamproject/AIPro/domain/member/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class ChatController {
 
     private final ChatService chatService;
