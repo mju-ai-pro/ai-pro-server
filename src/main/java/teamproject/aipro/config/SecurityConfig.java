3b6acbd70cded8f800968789f0f657fb1d979071
hoon <csh3004@naver.com>
2024-10-28 16:32:08 +0900

    fix/csrf

--- src/main/java/teamproject/aipro/config/SecurityConfig.java
+++ src/main/java/teamproject/aipro/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import teamproject.aipro.config.jwt.JwtAuthenticationFilter;
 
@@ -28,17 +27,11 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http,
-		JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf
-				.ignoringRequestMatchers("/api/**")
-			)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(requests -> requests
-				.requestMatchers("/api/**").permitAll()
-				.anyRequest().authenticated())
-			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+				.anyRequest().permitAll());
 		return http.build();
 	}
 
