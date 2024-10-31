6b162423871e36ed59f2ff358f0b69685bf57b9b
hoon <csh3004@naver.com>
2024-10-31 13:52:07 +0900

    fix/SecurityConfig

--- src/main/java/teamproject/aipro/config/SecurityConfig.java
+++ src/main/java/teamproject/aipro/config/SecurityConfig.java
@@ -29,6 +29,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.cors()
+			.and()
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(requests -> requests
 				.anyRequest().permitAll());
