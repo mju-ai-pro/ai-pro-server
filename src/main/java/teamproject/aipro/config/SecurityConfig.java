847cd7ba095630c101a80c7755af3c24d8066c37
hoon <csh3004@naver.com>
2024-10-31 14:23:24 +0900

    fix/cors

--- src/main/java/teamproject/aipro/config/SecurityConfig.java
+++ src/main/java/teamproject/aipro/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf(csrf -> csrf.disable())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(requests -> requests
 				.anyRequest().permitAll());
