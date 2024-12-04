5be7d38e8dfacfff277e4d1da197b7164c859a8c
hoon <csh3004@naver.com>
2024-12-04 14:21:50 +0900

    test : member, role 테스트 코드 추가

--- src/main/java/teamproject/aipro/domain/member/entity/Member.java
+++ src/main/java/teamproject/aipro/domain/member/entity/Member.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Member {
 
