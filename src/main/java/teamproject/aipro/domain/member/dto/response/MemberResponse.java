9163ff6ea11b7862994f3da2513de517500aac9e
hoon <csh3004@naver.com>
2024-12-04 14:21:50 +0900

    test : member, role 테스트 코드 추가

--- src/main/java/teamproject/aipro/domain/member/dto/response/MemberResponse.java
+++ src/main/java/teamproject/aipro/domain/member/dto/response/MemberResponse.java
@@ -2,8 +2,10 @@ package teamproject.aipro.domain.member.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class MemberResponse {
 
