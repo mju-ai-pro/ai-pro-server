515bc52ccdd213ded6b50379ce2567c8f55cda7f
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-12-11 19:06:00 +0900

    fix: allowed specific domains.

--- src/main/java/teamproject/aipro/domain/member/repository/MemberRepository.java
+++ src/main/java/teamproject/aipro/domain/member/repository/MemberRepository.java
@@ -1,10 +1,8 @@
 package teamproject.aipro.domain.member.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import teamproject.aipro.domain.member.entity.Member;
 
