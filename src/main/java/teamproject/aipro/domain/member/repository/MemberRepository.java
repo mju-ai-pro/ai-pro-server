7921a0e2cfd84c861787d20164f562164f2fad1c
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-12-11 18:10:03 +0900

    fix: added duplication remover in MemberService.

--- src/main/java/teamproject/aipro/domain/member/repository/MemberRepository.java
+++ src/main/java/teamproject/aipro/domain/member/repository/MemberRepository.java
@@ -1,11 +1,18 @@
 package teamproject.aipro.domain.member.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import teamproject.aipro.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-	Optional<Member> findByuserid(String id);
+	Optional<Member> findByUserid(String id);
+
+	@Query("SELECT m.userid FROM Member m GROUP BY m.userid HAVING COUNT(m.userid) > 1")
+	List<String> findDuplicateUserIds();
+
+	List<Member> findAllByUserid(String userid);
 }
