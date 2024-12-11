32d8f26e18ade08f39c669255622fb0f392f03b7
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-12-11 12:41:52 +0900

    fix: restored RestTemplate injection.

--- src/main/java/teamproject/aipro/config/WebConfig.java
+++ src/main/java/teamproject/aipro/config/WebConfig.java
@@ -1,18 +1,16 @@
 package teamproject.aipro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(@NonNull CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOriginPatterns("*")
-			.allowedMethods("*")
-			.allowedHeaders("*")
-			.allowCredentials(true);
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
