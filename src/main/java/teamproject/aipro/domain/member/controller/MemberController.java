239dafc14be2aa9165731aa2a53eef4ad7d23d77
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-12-11 19:46:34 +0900

    fix: corrected url-s.

--- src/main/java/teamproject/aipro/domain/member/controller/MemberController.java
+++ src/main/java/teamproject/aipro/domain/member/controller/MemberController.java
@@ -1,12 +1,9 @@
 package teamproject.aipro.domain.member.controller;
 
 import java.security.Principal;
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
