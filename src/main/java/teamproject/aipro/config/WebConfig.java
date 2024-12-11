366a0bb747e329d8e710ac608b2476c2098d2802
OxyToxi <50832867+OxyToxi@users.noreply.github.com>
2024-12-11 11:24:55 +0900

    fix: reverted config files to 0f2e560.

--- src/main/java/teamproject/aipro/config/WebConfig.java
+++ src/main/java/teamproject/aipro/config/WebConfig.java
@@ -1,16 +1,18 @@
 package teamproject.aipro.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	@Override
+	public void addCorsMappings(@NonNull CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true);
 	}
 }
